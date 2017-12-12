7d66d7e88791e716d2da46415e505aab71408629
Rituj Kumar <ritujkumar@hotmail.com>
2017-12-12 11:41:31 +0530

    loading data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import { Car } from './classes/car.js';
 import { Drone } from './classes/drone.js';
+import { fleet } from './fleetData.js';
+import { FleetDataService } from './services/fleetDataService.js';
 
-let c = new Car();
-let d = new Drone();
+let dataService  = new FleetDataService();
+dataService.loadData(fleet);
 
-console.log(c);
-console.log(d);
+console.log(dataService.cars);
+console.log(dataService.drones);
