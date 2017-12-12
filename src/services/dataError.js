a82531152ea89f1572f055cb69bf6eb95f1dd2f0
Rituj Kumar <ritujkumar@hotmail.com>
2017-12-12 12:50:40 +0530

    Handling errors

---
+++ src/services/dataError.js
@@ -0,0 +1,6 @@
+export class DataError {
+  constructor(mesage, data) {
+    thsi.message = message;
+    this.data  = data;
+  }
+}
--- src/services/fleetDataService.js
+++ src/services/fleetDataService.js
@@ -1,11 +1,13 @@
 import { Car } from '../classes/car.js';
 import { Drone } from '../classes/drone.js';
 import { fleet } from '../fleetData.js';
+import { DataError } from './dataError.js';
 
 export class FleetDataService {
   constructor() {
     this.cars = [];
     this.drones = [];
+    this.errors = [];
   }
 
   loadData(fleet) {
